c22780706fb3f014807c7d591b517e77fbe495d0
zelayab <bernardo_zelaya@hotmail.com>
2024-02-19 22:41:06 -0300

    Add Mantine color scheme script and provider

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@mantine/core";
+
+const TestComponent = () => {
+  return (
+    <div>
+      <h1>¡Mantine está funcionando!</h1>
+      <Button>Hello, Mantine</Button>
+    </div>
+  );
+};
+
+export default TestComponent;
