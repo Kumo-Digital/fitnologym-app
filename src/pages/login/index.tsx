e304506813c6e570f15fe12375e3c356f7020295
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-02-28 00:08:36 -0300

    Update the markup to not use the main tag, now its used in the root layout component

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,9 +10,9 @@ export default function Login() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <div>
         <LoginForm />
-      </main>
+      </div>
     </>
   );
 }
