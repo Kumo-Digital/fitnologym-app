a5a655fc852a4584031ea441847d53b47f5d7bb9
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-02-28 00:16:09 -0300

    Add nav links

--- src/components/ui/navbar/navlinks/navlinks.tsx
+++ src/components/ui/navbar/navlinks/navlinks.tsx
@@ -1,8 +1,19 @@
-import { ActionIcon, Avatar, Group } from "@mantine/core";
+import { ActionIcon, Avatar, Group, Text } from "@mantine/core";
+import Link from "next/link";
 
 export const NavLinks = () => {
   return (
-    <Group>
+    <Group gap={16}>
+      <Link href="#">
+        <Text size="sm" c="gray.5" px={16}>
+          Contacto
+        </Text>
+      </Link>
+      <Link href="#">
+        <Text size="sm" c="gray.5" px={16}>
+          Admin
+        </Text>
+      </Link>
       <ActionIcon variant="transparent" aria-label="Settings" size="xl">
         <Avatar radius="md" />
       </ActionIcon>
