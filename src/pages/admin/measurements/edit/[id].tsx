63903a4dc8a5721a381b7154f2d3b4249bdf0303
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-02-29 20:25:57 -0300

    Fix type issues

--- src/pages/admin/measurements/edit/[id].tsx
+++ src/pages/admin/measurements/edit/[id].tsx
@@ -2,57 +2,62 @@ import { validateRequest } from "@/lib/auth";
 import { useRouter } from "next/router";
 
 import type {
-	GetServerSidePropsContext,
-	GetServerSidePropsResult,
-	InferGetServerSidePropsType
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+  InferGetServerSidePropsType,
 } from "next";
 import type { User } from "lucia";
 import type { FormEvent } from "react";
 import { API_URL_V1, apiUrls } from "@/lib/apiUrls";
 
-export async function getServerSideProps(context: GetServerSidePropsContext): Promise<
-	GetServerSidePropsResult<{
-		user: User;
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+): Promise<
+  GetServerSidePropsResult<{
+    user: User;
     id: number;
-	}>
+  }>
 > {
   console.log(context.req.cookies);
-  const { id } = context.params;
-  console.log('Editar la Medida con ID:', id);
-	const { user } = await validateRequest(context.req, context.res);
-	console.log(user);
-	if (!user || !id) {
-		return {
-			redirect: {
-				permanent: false,
-				destination: "/login"
-			}
-		};
-	}
-  if (user.role !== 'administrator') {
-		console.log('NO PUEDES ESTAR AQUI!!!');
-		return {
-			redirect: {
-				permanent: false,
-				destination: "/"
-			}
-		};
-	}
-	return {
-		props: {
-			user,
-      id,
-		}
-	};
+  const { id } = context.params as { id: string };
+
+  console.log("Editar la Medida con ID:", id);
+  const { user } = await validateRequest(context.req, context.res);
+  console.log(user);
+  if (!user || !id) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login",
+      },
+    };
+  }
+  if (user.role !== "administrator") {
+    console.log("NO PUEDES ESTAR AQUI!!!");
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    };
+  }
+  return {
+    props: {
+      user,
+      id: parseInt(id),
+    },
+  };
 }
 
-export default function Page({ user, id }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-
-	return (
-		<>
-			<h1>You want the measure: {id}</h1>
-			<p>Your user ID is {user.id}.</p>
-			<p>You are a {user.role} user.</p>
-		</>
-	);
+export default function Page({
+  user,
+  id,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <>
+      <h1>You want the measure: {id}</h1>
+      <p>Your user ID is {user.id}.</p>
+      <p>You are a {user.role} user.</p>
+    </>
+  );
 }
