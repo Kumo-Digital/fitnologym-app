b127ad34458ab0f0470a428dbcd55010b5c76910
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-02-29 14:48:54 -0300

    Init breadcrumb component

---
+++ src/pages/mock/breadcrumb/index.tsx
@@ -0,0 +1,10 @@
+import { FitBreadcrumb } from "@/components/ui/fit-breadcrumb/fit-breadcrumb";
+
+export default function MockBreadcrumb() {
+  return (
+    <>
+      <h1>Breadcrumb Test</h1>
+      <FitBreadcrumb />
+    </>
+  );
+}
