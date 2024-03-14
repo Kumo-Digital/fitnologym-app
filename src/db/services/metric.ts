9adff4eb0e1d0e313ca8420b472850ba1a0d4be4
nabiedma <nabiedma@gmail.com>
2024-03-13 23:06:52 -0300

    add array to check the metric and generate the aggregate

--- src/db/services/metric.ts
+++ src/db/services/metric.ts
@@ -2,44 +2,61 @@ import connectDB from "@/lib/db";
 import { NextResponse } from "next/server";
 import MeasurementModel from "../models/MeasurementModel";
 
+const multipleValues = ['body_fat', 'muscle_mass', 'muscle_quality'];
+const circumferenceValues = ['circumference'];
+// const singleValues = ['weight', 'bmi', 'visc_fat', 'bone_mass', 'bmr', 'metab_age', 'body_water', 'physique_rating'];
+
 class MetricService {
   async getMetrics(userId: string, metric: string, startDate: string, endDate: string): Promise<any> {
+    let metrics: any[];
 
-    const metrics = await MeasurementModel.aggregate([
-      {
-        $match: {
-          user_id: userId,
-          createdAt: {
-            $gte: new Date(startDate),
-            $lte: new Date(endDate),
+    if (multipleValues.includes(metric)) {
+      metrics = [];
+    } else if (circumferenceValues.includes(metric)) {
+      metrics = [];
+    } else {
+      metrics = await MeasurementModel.aggregate([
+        {
+          $match: {
+            user_id: userId,
+            createdAt: {
+              $gte: new Date(startDate),
+              $lte: new Date(endDate),
+            }
           }
-        }
-      },
-      {
-        $project: {
-          _id: 0,
-          uom: `$values.${metric}.measure_uom`,
-          filtered_metrics: {
-            date: "$createdAt",
-            overview: `$values.${metric}.measure_value`
-          }
-        }
-      },
-      {
-        $group: {
-          _id: "$user_id",
-          uom: { $first: "$uom" },
-          filtered_metrics: { $push: "$filtered_metrics" }
         },
-      },
-      {
-        $project: {
-          _id: 0,
-          uom: 1,
-          filtered_metrics: 1
+        {
+          $project: {
+            _id: 0,
+            uom: `$values.${metric}.measure_uom`,
+            filtered_metrics: {
+              date: "$createdAt",
+              overview: `$values.${metric}.measure_value`
+            }
+          }
+        },
+        {
+          $unwind: '$filtered_metrics',
+        },
+        {
+          $sort: { 'filtered_metrics.date': 1 }
+        },
+        {
+          $group: {
+            _id: "$user_id",
+            uom: { $first: "$uom" },
+            filtered_metrics: { $push: "$filtered_metrics" }
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            uom: 1,
+            filtered_metrics: 1
+          }
         }
-      }
-    ]);
+      ]);
+    }
 
     console.log(metrics);
     return metrics;
