d838717e3163010f4dffd2161001ea6effd804e0
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-03-16 16:35:34 -0300

    Add searchbar

--- src/components/admin/users-tab/users-tab.tsx
+++ src/components/admin/users-tab/users-tab.tsx
@@ -1,10 +1,40 @@
 import { useUsers } from "@/hooks/users";
 import { Container, SimpleGrid, Skeleton, Stack } from "@mantine/core";
+import { useState } from "react";
+
+const sortOptions = [
+  { value: "fullname", label: "Nombre" },
+  { value: "gym", label: "Gimnasio" },
+  { value: "date", label: "Fecha de Creación" },
+];
 
 const UsersTab = () => {
   const { users, isLoading } = useUsers({ but: "admins" });
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [sortInput, setSortInput] = useState<string>("date");
 
-  console.log("users", users);
+  const handleSearch = (value: string) => {
+    setSearchInput(value);
+  };
+  const handleSort = (value: string) => {
+    setSortInput(value);
+  };
+
+  // filter and sort users based on search and sort input
+  const filteredUsers = users
+    ?.filter((user) => {
+      const regex = new RegExp(searchInput, "i");
+      return regex.test(user.fullname);
+    })
+    .sort((a, b) => {
+      if (sortInput === "name") {
+        return a.name.localeCompare(b.name);
+      }
+      if (sortInput === "date") {
+        return new Date(b.date).getTime() - new Date(a.date).getTime();
+      }
+      return 0;
+    });
 
   if (isLoading) return <p>loading ...</p>;
   return (
