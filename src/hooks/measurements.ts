db8f973a23910a408857519bb705ec4d35ed4e41
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-03-16 22:10:21 -0300

    Add measurements hook

--- src/hooks/measurements.ts
+++ src/hooks/measurements.ts
@@ -1,4 +1,5 @@
 import { apiUrls } from "@/lib/apiUrls";
+import { Measurement } from "@/types/measurements";
 import { apiFetcher } from "@/utils/apiUtils";
 import useSWR from "swr";
 
@@ -10,6 +11,27 @@ export const useUniqueLastMeasure = (userId: string): any => {
 
   return {
     lastMeasure: data,
+    error,
+    isLoading,
+    refetch: mutate,
+  };
+};
+
+interface UseMeasurementsReturn {
+  measurements: Measurement[];
+  error: any;
+  isLoading: boolean;
+  refetch: () => Promise<any>;
+}
+
+export const useMeasurements = (): UseMeasurementsReturn => {
+  const { data, error, isLoading, mutate } = useSWR(
+    apiUrls.measurements.get,
+    apiFetcher
+  );
+
+  return {
+    measurements: data,
     error,
     isLoading,
     refetch: mutate,
