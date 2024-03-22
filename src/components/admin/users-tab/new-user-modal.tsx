74aad4af1934a6c7224bfe1f40dd9c3287ec9558
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-03-22 19:28:32 -0300

    Updated previous modals to use the modals provider

--- src/components/admin/users-tab/new-user-modal.tsx
+++ src/components/admin/users-tab/new-user-modal.tsx
@@ -10,7 +10,6 @@ import {
   Button,
   Divider,
   Group,
-  Modal,
   NumberInput,
   Radio,
   Select,
@@ -24,7 +23,6 @@ import { until } from "@open-draft/until";
 import { Form, Formik } from "formik";
 
 interface NewUserModalProps {
-  isOpen: boolean;
   close: () => void;
   gyms: Gym[];
   refetch: () => void;
@@ -55,7 +53,7 @@ const renderSelectOption: SelectProps["renderOption"] = ({ option }) => (
   </Group>
 );
 
-const NewUserModal = ({ isOpen, close, gyms, refetch }: NewUserModalProps) => {
+const NewUserModal = ({ close, gyms, refetch }: NewUserModalProps) => {
   const gymOptions = gyms.map((gym) => ({ value: gym.id, label: gym.name }));
   const userTypeOptions = [
     { value: UserType.BASIC, label: "Básico" },
@@ -63,175 +61,168 @@ const NewUserModal = ({ isOpen, close, gyms, refetch }: NewUserModalProps) => {
     { value: UserType.PREMIUM, label: "Premium" },
   ];
   return (
-    <Modal
-      opened={isOpen}
-      onClose={close}
-      title="Agregar Nuevo Usuario"
-      centered
-    >
-      <Formik
-        initialValues={newUserInitialValues}
-        validationSchema={newUserFormValidationSchema}
-        onSubmit={async (values) => {
-          const { data, error } = await until(() => createUser(values));
+    <Formik
+      initialValues={newUserInitialValues}
+      validationSchema={newUserFormValidationSchema}
+      onSubmit={async (values) => {
+        const { data, error } = await until(() => createUser(values));
 
-          if (error) {
-            notifications.show({
-              title: "Error",
-              message: "Ocurrió un error al crear el usuario",
-              color: "red",
-            });
-            close();
-            return;
-          }
-
+        if (error) {
           notifications.show({
-            title: "Usuario Creado",
-            message: `El usuario ${data.fullname} ha sido creado exitosamente`,
-            color: "lime",
+            title: "Error",
+            message: "Ocurrió un error al crear el usuario",
+            color: "red",
           });
-          refetch();
           close();
-        }}
-      >
-        {({
-          values,
-          handleChange,
-          handleBlur,
-          touched,
-          errors,
-          setFieldValue,
-          setFieldTouched,
-          handleSubmit,
-        }) => (
-          <Form onSubmit={handleSubmit}>
-            <Stack gap={16}>
+          return;
+        }
+
+        notifications.show({
+          title: "Usuario Creado",
+          message: `El usuario ${data.fullname} ha sido creado exitosamente`,
+          color: "lime",
+        });
+        refetch();
+        close();
+      }}
+    >
+      {({
+        values,
+        handleChange,
+        handleBlur,
+        touched,
+        errors,
+        setFieldValue,
+        setFieldTouched,
+        handleSubmit,
+      }) => (
+        <Form onSubmit={handleSubmit}>
+          <Stack gap={16}>
+            <TextInput
+              name="fullname"
+              label="Nombre Completo"
+              placeholder="Nombre y Apellido"
+              value={values.fullname}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={touched.fullname && errors.fullname}
+            />
+            <TextInput
+              name="email"
+              label="Correo Electrónico"
+              placeholder="Dirección de Correo Electrónico"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={touched.email && errors.email}
+            />
+            <Group gap={16} align="stretch">
               <TextInput
-                name="fullname"
-                label="Nombre Completo"
-                placeholder="Nombre y Apellido"
-                value={values.fullname}
+                name="dni"
+                label="DNI"
+                placeholder="Documento Nacional de Identidad"
+                value={values.dni}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                error={touched.fullname && errors.fullname}
+                error={touched.dni && errors.dni}
+                flex={"1 0 0"}
               />
-              <TextInput
-                name="email"
-                label="Correo Electrónico"
-                placeholder="Dirección de Correo Electrónico"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.email && errors.email}
-              />
-              <Group gap={16} align="stretch">
-                <TextInput
-                  name="dni"
-                  label="DNI"
-                  placeholder="Documento Nacional de Identidad"
-                  value={values.dni}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  error={touched.dni && errors.dni}
-                  flex={"1 0 0"}
-                />
-                <Select
-                  name="user_type"
-                  label="Suscripción"
-                  placeholder="Seleccionar Suscripción"
-                  data={userTypeOptions}
-                  renderOption={renderSelectOption}
-                  leftSection={
-                    <Box
-                      w={8}
-                      h={8}
-                      bg={getSubscriptionColor(values.user_type)}
-                      style={{
-                        borderRadius: "100%",
-                      }}
-                    ></Box>
-                  }
-                  value={values.user_type}
-                  onChange={(e) => setFieldValue("user_type", e)}
-                  onBlur={() => setFieldTouched("user_type", true)}
-                  error={touched.user_type && errors.user_type}
-                  allowDeselect={false}
-                  withCheckIcon={false}
-                />
-              </Group>
               <Select
-                name="gym_id"
-                label="Gimnasio"
-                placeholder="Seleccionar Gimnasio"
-                data={gymOptions}
-                value={values.gym_id}
-                onChange={(e) => setFieldValue("gym_id", e)}
-                onBlur={() => setFieldTouched("gym_id", true)}
-                error={touched.gym_id && errors.gym_id}
+                name="user_type"
+                label="Suscripción"
+                placeholder="Seleccionar Suscripción"
+                data={userTypeOptions}
+                renderOption={renderSelectOption}
+                leftSection={
+                  <Box
+                    w={8}
+                    h={8}
+                    bg={getSubscriptionColor(values.user_type)}
+                    style={{
+                      borderRadius: "100%",
+                    }}
+                  ></Box>
+                }
+                value={values.user_type}
+                onChange={(e) => setFieldValue("user_type", e)}
+                onBlur={() => setFieldTouched("user_type", true)}
+                error={touched.user_type && errors.user_type}
+                allowDeselect={false}
+                withCheckIcon={false}
               />
-              <Radio.Group
-                name="gender"
-                label="Género"
-                value={values.gender}
-                onChange={(value) => setFieldValue("gender", value)}
-              >
-                <Group gap={32} mt="xs">
-                  <Radio value="male" label="Hombre" />
-                  <Radio value="female" label="Mujer" />
-                </Group>
-              </Radio.Group>
-              <Divider />
-              <Stack gap={4}>
-                <Text>Objetivos del Usuario</Text>
-                <Text size="sm">
-                  Define metas específicas y medibles adaptadas a tu trayectoria
-                  fitness. Haz un seguimiento del progreso, mantente motivado y
-                  conquista tus aspiraciones fitness con precisión y claridad.
-                </Text>
-              </Stack>
-              <Group gap={16} align="stretch">
-                <Select
-                  name="target_metric"
-                  label="Métrica Objetivo"
-                  placeholder="Seleccionar una Métrica"
-                  disabled
-                  data={[{ label: "Peso", value: "weight" }]}
-                  value={values.target_metric}
-                  onChange={(e) => setFieldValue("target_metric", e)}
-                  onBlur={() => setFieldTouched("target_metric", true)}
-                  error={touched.target_metric && errors.target_metric}
-                  flex="1"
-                />
-                <NumberInput
-                  name="target_value"
-                  label="Objetivo"
-                  placeholder="Kgs"
-                  value={values.target_value}
-                  onChange={(e) => setFieldValue("target_value", e)}
-                  onBlur={() => setFieldTouched("target_value", true)}
-                  error={touched.target_value && errors.target_value}
-                  min={0}
-                  rightSection={
-                    <Text size="sm" mr={4}>
-                      Kg
-                    </Text>
-                  }
-                  w={150}
-                />
+            </Group>
+            <Select
+              name="gym_id"
+              label="Gimnasio"
+              placeholder="Seleccionar Gimnasio"
+              data={gymOptions}
+              value={values.gym_id}
+              onChange={(e) => setFieldValue("gym_id", e)}
+              onBlur={() => setFieldTouched("gym_id", true)}
+              error={touched.gym_id && errors.gym_id}
+            />
+            <Radio.Group
+              name="gender"
+              label="Género"
+              value={values.gender}
+              onChange={(value) => setFieldValue("gender", value)}
+            >
+              <Group gap={32} mt="xs">
+                <Radio value="male" label="Hombre" />
+                <Radio value="female" label="Mujer" />
               </Group>
-              <Group justify="flex-end">
-                <Button variant="subtle" onClick={close}>
-                  Cancelar
-                </Button>
-                <Button type="submit" c="black">
-                  Agregar
-                </Button>
-              </Group>
+            </Radio.Group>
+            <Divider />
+            <Stack gap={4}>
+              <Text>Objetivos del Usuario</Text>
+              <Text size="sm" c="gray.5">
+                Define metas específicas y medibles adaptadas a tu trayectoria
+                fitness. Haz un seguimiento del progreso, mantente motivado y
+                conquista tus aspiraciones fitness con precisión y claridad.
+              </Text>
             </Stack>
-          </Form>
-        )}
-      </Formik>
-    </Modal>
+            <Group gap={16} align="stretch">
+              <Select
+                name="target_metric"
+                label="Métrica Objetivo"
+                placeholder="Seleccionar una Métrica"
+                disabled
+                data={[{ label: "Peso", value: "weight" }]}
+                value={values.target_metric}
+                onChange={(e) => setFieldValue("target_metric", e)}
+                onBlur={() => setFieldTouched("target_metric", true)}
+                error={touched.target_metric && errors.target_metric}
+                flex="1"
+              />
+              <NumberInput
+                name="target_value"
+                label="Objetivo"
+                placeholder="Kgs"
+                value={values.target_value}
+                onChange={(e) => setFieldValue("target_value", e)}
+                onBlur={() => setFieldTouched("target_value", true)}
+                error={touched.target_value && errors.target_value}
+                min={0}
+                rightSection={
+                  <Text size="sm" mr={4}>
+                    Kg
+                  </Text>
+                }
+                w={150}
+              />
+            </Group>
+            <Group justify="flex-end">
+              <Button variant="subtle" onClick={close}>
+                Cancelar
+              </Button>
+              <Button type="submit" c="black">
+                Agregar
+              </Button>
+            </Group>
+          </Stack>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
--- src/components/admin/users-tab/users-tab.tsx
+++ src/components/admin/users-tab/users-tab.tsx
@@ -9,6 +9,7 @@ import { IconPlus } from "@tabler/icons-react";
 import { useDisclosure } from "@mantine/hooks";
 import NewUserModal from "./new-user-modal";
 import { parseDate } from "@/utils/utils";
+import { modals } from "@mantine/modals";
 
 const sortOptions = [
   { value: "fullname", label: "Nombre" },
@@ -21,7 +22,15 @@ const UsersTab = () => {
   const { gyms, isLoading: isLoadingGyms } = useGyms();
   const [searchInput, setSearchInput] = useState<string>("");
   const [sortInput, setSortInput] = useState<string>("date");
-  const [opened, { open, close }] = useDisclosure(false);
+
+  const newUserModal = () =>
+    modals.open({
+      title: "Agregar Nuevo Usuario",
+      centered: true,
+      children: (
+        <NewUserModal gyms={gyms} close={modals.closeAll} refetch={refetch} />
+      ),
+    });
 
   const handleSearch = (value: string) => {
     setSearchInput(value);
@@ -54,54 +63,45 @@ const UsersTab = () => {
 
   if (isLoading || isLoadingGyms) return <UsersTabSkeleton />;
   return (
-    <>
-      <Container size={1024}>
-        <Stack gap={24}>
-          <Group gap={16}>
-            <SearchBar
-              searchValue={searchInput}
-              sortValue={sortInput}
-              sortOptions={sortOptions}
-              handleSearch={handleSearch}
-              handleSort={handleSort}
-              searchPlaceholder="Buscar Usuarios..."
-            />
-            <Button
-              onClick={open}
-              size="sm"
-              variant="filled"
-              c="black"
-              rightSection={<IconPlus size={14} />}
-              w={150}
-            >
-              Agregar
-            </Button>
-          </Group>
-          <SimpleGrid cols={3} spacing={24} verticalSpacing={24}>
-            {filteredUsers.map((user) => {
-              const userGym = gyms.find((gym) => gym.id === user.gym_id);
-              const userDate = parseDate(user.createdAt);
-              return (
-                <UserCard
-                  key={user._id}
-                  title={user.fullname}
-                  subtitle={userGym?.name || "Sin Asignar"}
-                  description={`Se unió el ${userDate}`}
-                  link={`/${userGym!.id}/${user._id}`}
-                />
-              );
-            })}
-          </SimpleGrid>
-        </Stack>
-      </Container>
-
-      <NewUserModal
-        isOpen={opened}
-        close={close}
-        gyms={gyms}
-        refetch={refetch}
-      />
-    </>
+    <Container size={1024}>
+      <Stack gap={24}>
+        <Group gap={16}>
+          <SearchBar
+            searchValue={searchInput}
+            sortValue={sortInput}
+            sortOptions={sortOptions}
+            handleSearch={handleSearch}
+            handleSort={handleSort}
+            searchPlaceholder="Buscar Usuarios..."
+          />
+          <Button
+            onClick={() => newUserModal()}
+            size="sm"
+            variant="filled"
+            c="black"
+            rightSection={<IconPlus size={14} />}
+            w={150}
+          >
+            Agregar
+          </Button>
+        </Group>
+        <SimpleGrid cols={3} spacing={24} verticalSpacing={24}>
+          {filteredUsers.map((user) => {
+            const userGym = gyms.find((gym) => gym.id === user.gym_id);
+            const userDate = parseDate(user.createdAt);
+            return (
+              <UserCard
+                key={user._id}
+                title={user.fullname}
+                subtitle={userGym?.name || "Sin Asignar"}
+                description={`Se unió el ${userDate}`}
+                link={`/${userGym!.id}/${user._id}`}
+              />
+            );
+          })}
+        </SimpleGrid>
+      </Stack>
+    </Container>
   );
 };
 
