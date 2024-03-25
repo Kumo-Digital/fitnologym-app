2d758c34e4675f4c4cea57ea32bdb2234ee5f3de
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-03-25 09:47:24 -0300

    Add user and administrator login redirects

--- src/lib/appUrls.ts
+++ src/lib/appUrls.ts
@@ -1,7 +1,13 @@
 export const appUrls = {
+  auth: {
+    login: "/login",
+  },
   admin: "/admin",
   userDetails: (gymId: string, userId: string) => `/${gymId}/${userId}`,
   measurements: {
     new: "/admin/measurements/new",
+  },
+  user: {
+    my_profile: "/my-profile",
   },
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,57 +2,56 @@ import { validateRequest } from "@/lib/auth";
 import { useRouter } from "next/router";
 
 import type {
-	GetServerSidePropsContext,
-	GetServerSidePropsResult,
-	InferGetServerSidePropsType
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+  InferGetServerSidePropsType,
 } from "next";
 import type { User } from "lucia";
-import type { FormEvent } from "react";
-import { API_URL_V1, apiUrls } from "@/lib/apiUrls";
+import { appUrls } from "@/lib/appUrls";
 
-export async function getServerSideProps(context: GetServerSidePropsContext): Promise<
-	GetServerSidePropsResult<{
-		user: User;
-	}>
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+): Promise<
+  GetServerSidePropsResult<{
+    user: User;
+  }>
 > {
-  console.log(context.req.cookies);
-	const { user } = await validateRequest(context.req, context.res);
-	console.log(user);
-	if (!user) {
-		return {
-			redirect: {
-				permanent: false,
-				destination: "/login"
-			}
-		};
-	}
-	return {
-		props: {
-			user
-		}
-	};
+  const { user } = await validateRequest(context.req, context.res);
+
+  if (!user) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: appUrls.auth.login,
+      },
+    };
+  }
+
+  if (user.role === "administrator")
+    return {
+      redirect: {
+        permanent: false,
+        destination: appUrls.admin,
+      },
+    };
+
+  if (user.role === "user")
+    return {
+      redirect: {
+        permanent: false,
+        destination: appUrls.user.my_profile,
+      },
+    };
+
+  return {
+    props: {
+      user,
+    },
+  };
 }
 
-export default function Page({ user }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-	const router = useRouter();
-
-	async function onSubmit(e: FormEvent<HTMLFormElement>) {
-		e.preventDefault();
-		const formElement = e.target as HTMLFormElement;
-		await fetch(formElement.action, {
-			method: formElement.method
-		});
-		router.push("/login");
-	}
-
-	return (
-		<>
-			<h1>Hi, {user.fullname}!</h1>
-			<p>Your user ID is {user.id}.</p>
-			<p>You are a {user.user_type} user.</p>
-			<form method="post" action={`./${API_URL_V1}${apiUrls.auth.logout}`} onSubmit={onSubmit}>
-				<button>Sign out</button>
-			</form>
-		</>
-	);
+export default function Page({}: InferGetServerSidePropsType<
+  typeof getServerSideProps
+>) {
+  return <></>;
 }
