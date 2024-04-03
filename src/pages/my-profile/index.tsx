50f9cc9aad737c4807f057ad868fdf0d5a8ea19f
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-04-03 18:43:05 -0300

    Add responsive to my-profile

--- src/pages/my-profile/index.tsx
+++ src/pages/my-profile/index.tsx
@@ -5,7 +5,10 @@ import ResourcesTab from "@/components/user-overview/resources-tab/resources-tab
 import UserOverviewEmpty from "@/components/user-overview/user-overview-empty";
 import UserOverviewHeader from "@/components/user-overview/user-overview-header";
 import { UserOverviewSkeleton } from "@/components/user-overview/user-overview-skeleton";
-import { useCalculateEvolution, useUniqueLastMeasure } from "@/hooks/measurements";
+import {
+  useCalculateEvolution,
+  useUniqueLastMeasure,
+} from "@/hooks/measurements";
 import { useUniqueUser } from "@/hooks/users";
 import { validateRequest } from "@/lib/auth";
 import { User } from "@/types/user";
@@ -19,41 +22,47 @@ import WelcomeModal from "@/components/ui/modal/welcome-modal/welcome-modal";
 import { useEffect } from "react";
 import { saveLastLoggedInDate } from "@/services/users";
 
-export async function getServerSideProps(context: GetServerSidePropsContext): Promise<
-	GetServerSidePropsResult<{
-		sessionUser: User;
-	}>
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+): Promise<
+  GetServerSidePropsResult<{
+    sessionUser: User;
+  }>
 > {
-	const { user } = await validateRequest(context.req, context.res);
+  const { user } = await validateRequest(context.req, context.res);
 
-	if (!user) {
-		return {
-			redirect: {
-				permanent: false,
-				destination: "/login"
-			}
-		};
-	}
-
-  if (user.role === 'administrator') {
+  if (!user) {
     return {
-			redirect: {
-				permanent: false,
-				destination: "/admin"
-			}
-		};
+      redirect: {
+        permanent: false,
+        destination: "/login",
+      },
+    };
   }
 
-	return {
-		props: {
-			sessionUser: JSON.parse(JSON.stringify(user)),
-		}
-	} as any;
+  if (user.role === "administrator") {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/admin",
+      },
+    };
+  }
+
+  return {
+    props: {
+      sessionUser: JSON.parse(JSON.stringify(user)),
+    },
+  } as any;
 }
 
-const UserOverview: NextPageWithLayout<{ sessionUser: LuciaUser }> = ({ sessionUser }) => {
+const UserOverview: NextPageWithLayout<{ sessionUser: LuciaUser }> = ({
+  sessionUser,
+}) => {
   const { user, isLoading } = useUniqueUser({ id: sessionUser.id as string });
-  const { lastMeasure, isLoading: isLoadingLastMeasure } = useUniqueLastMeasure(sessionUser.id as string);
+  const { lastMeasure, isLoading: isLoadingLastMeasure } = useUniqueLastMeasure(
+    sessionUser.id as string
+  );
   const { evolution, isLoading: isLoadingEvolution } = useCalculateEvolution(
     sessionUser.id as string
   );
@@ -61,7 +70,7 @@ const UserOverview: NextPageWithLayout<{ sessionUser: LuciaUser }> = ({ sessionU
   const onLastLogin = async () => {
     modals.closeAll();
     await saveLastLoggedInDate(sessionUser.id as string);
-  }
+  };
 
   const welcomeModal = () =>
     modals.open({
@@ -72,9 +81,7 @@ const UserOverview: NextPageWithLayout<{ sessionUser: LuciaUser }> = ({ sessionU
       closeOnEscape: false,
       size: "lg",
       padding: "xl",
-      children: (
-        <WelcomeModal onLastLogin={onLastLogin} />
-      ),
+      children: <WelcomeModal onLastLogin={onLastLogin} />,
     });
 
   useEffect(() => {
@@ -84,24 +91,40 @@ const UserOverview: NextPageWithLayout<{ sessionUser: LuciaUser }> = ({ sessionU
   }, []);
 
   if (sessionUser.id === "undefined") return <UserOverviewEmpty />;
-  if (isLoading || isLoadingLastMeasure || isLoadingEvolution) return <UserOverviewSkeleton />;
+  if (isLoading || isLoadingLastMeasure || isLoadingEvolution)
+    return <UserOverviewSkeleton />;
   return (
     <Stack gap={16} style={{ flexGrow: 1 }}>
       {/* TAB LIST */}
-      <Tabs defaultValue="overview" keepMounted={false}>
-        <Tabs.List>
+      <Tabs
+        defaultValue="overview"
+        keepMounted={false}
+        style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}
+      >
+        <Tabs.List
+          style={{
+            flexWrap: "nowrap",
+            overflowX: "auto",
+          }}
+        >
           <Tabs.Tab value="overview">Overview</Tabs.Tab>
           <Tooltip
-            label={"El análisis estará disponible una vez que tenga más de 1 medida cargada en el sistema"}
+            label={
+              "El análisis estará disponible una vez que tenga más de 1 medida cargada en el sistema"
+            }
             multiline
             withArrow
             disabled={evolution}
           >
-            <Tabs.Tab value="analysis" disabled={!lastMeasure || !evolution}>Análisis</Tabs.Tab>
+            <Tabs.Tab value="analysis" disabled={!lastMeasure || !evolution}>
+              Análisis
+            </Tabs.Tab>
           </Tooltip>
-          <Tabs.Tab value="report" disabled={!lastMeasure}>Diagnóstico</Tabs.Tab>
+          <Tabs.Tab value="report" disabled={!lastMeasure}>
+            Diagnóstico
+          </Tabs.Tab>
           <Tabs.Tab value="exercise-plan" disabled>
-            <Group align="center" gap={8}>
+            <Group align="center" gap={8} wrap="nowrap">
               Mi Rutina
               <Badge variant="outline" color="lime">
                 Plus
@@ -109,7 +132,7 @@ const UserOverview: NextPageWithLayout<{ sessionUser: LuciaUser }> = ({ sessionU
             </Group>
           </Tabs.Tab>
           <Tabs.Tab value="diet-plan" disabled>
-            <Group align="center" gap={8}>
+            <Group align="center" gap={8} wrap="nowrap">
               Plan Nutricional
               <Badge variant="outline" color="lime">
                 Premium
@@ -123,7 +146,10 @@ const UserOverview: NextPageWithLayout<{ sessionUser: LuciaUser }> = ({ sessionU
         <UserOverviewHeader user={user} sessionUser={user} />
 
         {/* PANELS */}
-        <Tabs.Panel value="overview">
+        <Tabs.Panel
+          value="overview"
+          style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}
+        >
           <OverviewTab user={user} />
         </Tabs.Panel>
         <Tabs.Panel value="analysis">
