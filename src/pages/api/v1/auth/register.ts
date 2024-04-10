aaa25a54aa3cb8b7527cd4766beb030c017ac8fb
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-04-10 10:56:33 -0300

    Remove console.logs

--- src/pages/api/v1/auth/register.ts
+++ src/pages/api/v1/auth/register.ts
@@ -1,25 +1,27 @@
-import connectDB from '@/lib/db';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import bcrypt from 'bcrypt';
-import UserService from '@/db/services/user';
- 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+import connectDB from "@/lib/db";
+import type { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+import UserService from "@/db/services/user";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "POST") {
-		res.status(404).end();
-		return;
-	}
+    res.status(404).end();
+    return;
+  }
 
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     try {
-        await connectDB();
-        const userService = new UserService();
-        const res = await userService.createUser(req.body);
+      await connectDB();
+      const userService = new UserService();
+      const res = await userService.createUser(req.body);
 
-        if (res.ok) {
-            return await res.json();
-        }
-        console.log('Hubo un error');
-
+      if (res.ok) {
+        return await res.json();
+      }
+      console.error("Hubo un error");
     } catch (e) {
       console.error(e);
     }
