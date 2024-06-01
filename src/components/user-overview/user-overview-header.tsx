2f183672a87dc19090b87869ca629c0223d4e274
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-06-01 12:48:16 -0300

    Fix user overview loading skeleton

--- src/components/user-overview/user-overview-header.tsx
+++ src/components/user-overview/user-overview-header.tsx
@@ -2,7 +2,18 @@ import { useUniqueLastMeasure } from "@/hooks/measurements";
 import { appUrls } from "@/lib/appUrls";
 import { User } from "@/types/user";
 import { parseDate } from "@/utils/utils";
-import { Group, Stack, Title, Text, Button, Divider } from "@mantine/core";
+import {
+  Group,
+  Stack,
+  Title,
+  Text,
+  Button,
+  Divider,
+  em,
+  ActionIcon,
+  Box,
+} from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { IconPlus } from "@tabler/icons-react";
 import { useRouter } from "next/router";
 
@@ -13,41 +24,89 @@ const UserOverviewHeader = ({
   user: User;
   sessionUser: User;
 }) => {
+  const isMobileSM = useMediaQuery(`(max-width: ${em(425)})`);
+  const isMobileMD = useMediaQuery(`(max-width: ${em(800)})`);
+
   const { push } = useRouter();
 
   const { lastMeasure } = useUniqueLastMeasure(user._id);
 
   return (
     <Stack gap={0}>
-      <Group justify="space-between" mih={120} py={32}>
-        <Title order={1} size={32} c="gray.0">
-          {user.fullname}
-        </Title>
-        <Group>
-          {lastMeasure?.date ? (
-            <Text size="sm" c="gray.5">
-              La última medición fue realizada el {parseDate(lastMeasure.date)}
-            </Text>
-          ) : (
-            <Text size="sm" c="gray.5">
-              Sin mediciones a la fecha del dia de hoy
-            </Text>
-          )}
-          {sessionUser.role === "administrator" && (
-            <Button
-              c="black"
-              leftSection={<IconPlus />}
-              onClick={() =>
-                push(`${appUrls.measurements.new}?userId=${user._id}`)
-              }
-            >
-              Nueva Medición
-            </Button>
-          )}
-        </Group>
-      </Group>
+      <Box mih={120} py={32}>
+        {isMobileMD ? (
+          <Stack>
+            <Group align="center" justify="space-between" gap={8}>
+              <Title order={1} size={32} c="gray.0">
+                {user.fullname}
+              </Title>
+              {isMobileSM ? (
+                <ActionIcon
+                  variant="filled"
+                  aria-label="Nueva Medida"
+                  size="lg"
+                >
+                  <IconPlus
+                    style={{ width: "70%", height: "70%" }}
+                    stroke={1.5}
+                  />
+                </ActionIcon>
+              ) : (
+                <Button
+                  c="black"
+                  leftSection={<IconPlus />}
+                  onClick={() =>
+                    push(`${appUrls.measurements.new}?userId=${user._id}`)
+                  }
+                >
+                  Nueva Medición
+                </Button>
+              )}
+            </Group>
+            <MeasureLegend lastMeasure={lastMeasure} />
+          </Stack>
+        ) : (
+          <Group flex="1 0 0">
+            <Title order={1} size={32} c="gray.0" mr="auto">
+              {user.fullname}
+            </Title>
+            <Group>
+              <MeasureLegend lastMeasure={lastMeasure} />
+              {sessionUser.role === "administrator" && (
+                <Button
+                  c="black"
+                  leftSection={<IconPlus />}
+                  onClick={() =>
+                    push(`${appUrls.measurements.new}?userId=${user._id}`)
+                  }
+                >
+                  Nueva Medición
+                </Button>
+              )}
+            </Group>
+          </Group>
+        )}
+      </Box>
       <Divider size="sm" />
     </Stack>
+  );
+};
+
+const MeasureLegend = ({ lastMeasure }: { lastMeasure: any }) => {
+  const isMobileSM = useMediaQuery(`(max-width: ${em(425)})`);
+
+  return (
+    <>
+      {lastMeasure?.date ? (
+        <Text size="sm" c="gray.5" ta={isMobileSM ? "left" : "right"}>
+          La última medición fue realizada el {parseDate(lastMeasure.date)}
+        </Text>
+      ) : (
+        <Text size="sm" c="gray.5">
+          Sin mediciones a la fecha del dia de hoy
+        </Text>
+      )}
+    </>
   );
 };
 
