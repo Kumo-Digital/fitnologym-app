9bceaba728c41986e28617c8c08b25ed19fe2f4b
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-06-16 11:23:49 -0300

    Add FFMI Balance Component

---
+++ src/components/ui/ffmi-balance/index.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useRef } from "react";
+import classes from "./ffmi-balance.module.css";
+import { FFMIBalanceProps } from "./ffmi-balance.types";
+import { mapRange } from "./ffmi-balance.utils";
+
+const FFMIBalance = ({
+  label,
+  description,
+  value,
+  styles,
+  ...props
+}: FFMIBalanceProps) => {
+  const valueRef = useRef<HTMLMeterElement>(null);
+
+  const mappedValue = mapRange(value);
+
+  useEffect(() => {
+    if (valueRef.current !== null) {
+      valueRef.current.style.setProperty(
+        "--indicator-position",
+        `${mappedValue}%`
+      );
+    }
+  }, [value, valueRef]);
+
+  return (
+    <div className={classes.root} style={styles} {...props}>
+      <div className={classes.header}>
+        {label && (
+          <label htmlFor="meter" className={classes.label}>
+            {label}
+          </label>
+        )}
+        {description && <p className={classes.description}>{description}</p>}
+      </div>
+      <div className={classes.graph}>
+        <meter
+          ref={valueRef}
+          id="meter"
+          min="0"
+          max="100"
+          value={mappedValue}
+          className={classes.meter}
+        ></meter>
+        <div className={classes.values}>
+          {Array.from({ length: 15 }).map((_, index) => (
+            <span key={`value-${index}`}>{16 + index}</span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FFMIBalance;
