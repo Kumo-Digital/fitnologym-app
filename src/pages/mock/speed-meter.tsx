c9cfac36541206315a4a788debd451abcf869934
mauricarron <59117899+mauricarron@users.noreply.github.com>
2024-06-16 19:52:03 -0300

    Update page to test body section component

--- src/pages/mock/speed-meter.tsx
+++ src/pages/mock/speed-meter.tsx
@@ -1,4 +1,5 @@
 import SpeedMeter from "@/components/ui/speed-meter";
+import BodySectionBalance from "@/components/user-overview/overview-tab/body-section-balance";
 import { withRootLayout } from "@/utils/layouts";
 import { Container, Group, Stack, Title } from "@mantine/core";
 import React from "react";
@@ -8,10 +9,18 @@ const SpeedMeterPage = () => {
     // <Container>
     <Stack gap={32}>
       <Title>SpeedMeterPage</Title>
-      <Group wrap="nowrap" w={500} h={500}>
-        <SpeedMeter name="Brazos" value={0} />
-        <SpeedMeter name="Piernas" value={0} />
-      </Group>
+      <BodySectionBalance
+        name="Grasa Corporal"
+        description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        armsValue={5}
+        legsValue={-3}
+      />
+      <BodySectionBalance
+        name="Masa Muscular"
+        armsValue={7}
+        legsValue={0}
+        description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
     </Stack>
     // </Container>
   );
